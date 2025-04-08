e3276f6abf7ced5daf7ef1de09d920edbacc1a61
yuzumone <yuzumone@outlook.com>
2025-04-08 21:54:04 +0900

    Add license page

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:fwf/ui/home/home_notifier.dart';
 import 'package:fwf/ui/list/list_page.dart';
 import 'package:fwf/ui/map/map_page.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+
+const _legalease = '''このアプリは株式会社オリエンタルランドまたウォルト・ディズニー・カンパニーとは関係がありません。
+メニュー画像は東京ディズニーリゾート公式 Web ページからの引用です。''';
 
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
@@ -21,7 +26,32 @@ class HomePage extends ConsumerWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
       home: Scaffold(
-        appBar: AppBar(title: const Text('Maihama Food Guide')),
+        appBar: AppBar(
+          title: const Text('Maihama Food Guide'),
+          actions: [
+            PopupMenuButton(
+              onSelected: (_) {
+                PackageInfo.fromPlatform().then(
+                  (value) => showAboutDialog(
+                    context: context,
+                    applicationName: value.appName,
+                    applicationVersion: value.version,
+                    applicationIcon: SvgPicture.asset(
+                      'assets/icon.svg',
+                      width: 48.0,
+                      height: 48.0,
+                    ),
+                    applicationLegalese: _legalease,
+                  ),
+                );
+              },
+              itemBuilder:
+                  (_) => const [
+                    PopupMenuItem(value: 0, child: Text('License')),
+                  ],
+            ),
+          ],
+        ),
         body: switch (shops) {
           AsyncData(:final value) => switch (pageIndex) {
             0 => ListPage(shops: value),
