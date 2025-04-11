6afb4503ccd7755fa1ea5b5eaca973fcb75acab6
yuzumone <yuzumone@outlook.com>
2025-04-11 19:06:46 +0900

    Update test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,43 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:fwf/data/model/event.dart';
+import 'package:fwf/data/model/menu.dart';
+import 'package:fwf/data/model/shop.dart';
+import 'package:fwf/data/repository/asset_repository.dart';
 
 import 'package:fwf/main.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockAssetRepository extends Mock implements AssetRepository {}
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('App widget test', (tester) async {
+    await tester.pumpWidget(
+      ProviderScope(
+        overrides: [
+          assetRepositoryProvider.overrideWith((ref) {
+            final mock = MockAssetRepository();
+            when(() => mock.getEvents()).thenAnswer(
+              (_) async => [Event(name: 'a', start: 0, end: 1, file: 'a')],
+            );
+            when(() => mock.getShops('a')).thenAnswer(
+              (_) async => [
+                Shop(
+                  name: 'a',
+                  aria: 'a',
+                  lat: 0.0,
+                  lng: 0.0,
+                  menus: [
+                    Menu(name: 'a', value: 1, link: 'a', text: 'a', image: 'a'),
+                  ],
+                ),
+              ],
+            );
+            return mock;
+          }),
+        ],
+        child: App(),
+      ),
+    );
   });
 }
