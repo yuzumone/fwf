81c7db6388c016a6ad24881061063c63534921a5
yuzumone <yuzumone@outlook.com>
2026-03-17 16:46:28 +0900

    flutter pub upgrade --major-versions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
 
     defaultConfig {
         applicationId = "net.yuzumone.fwf"
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
